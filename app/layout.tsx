3eece7b51dc467394191b288f1b613d68c3950ac
Jonathan Grillo <grillo.jonathan@gmail.com>
2024-12-04 14:07:36 +0100

    feat: finish tutorial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  icons: ['favicon.ico']
+};
 
 export default function RootLayout({
   children,
@@ -8,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="favicon.ico" />
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
